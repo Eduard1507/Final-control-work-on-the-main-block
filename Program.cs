bd1c973003e079d25dc82f8a4bab6614b1d790ba
Eduard <Eduard15.07@mail.ru>
2024-04-08 23:47:33 +0300

    Перенес програаму в дргугой файл

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿string[] FilterStrings(string[] array)
+{
+    int count = 0;
+    foreach (string str in array)
+    {
+        if (str.Length <= 3)
+        {
+            count++;
+        }
+    }
+    string[] filteredArray = new string[count];
+    int index = 0;
+
+    foreach (string str in array)
+    {
+        if (str.Length <= 3)
+        {
+            filteredArray[index] = str;
+            index++;
+        }
+    }
+
+    return filteredArray;
+}
+{
+    string[] inputArray = { "apple", "banana", "cat", "dog", "elephant" };
+    string[] filteredArray = FilterStrings(inputArray);
+    Console.WriteLine("Новый массив из строк, длина которых меньше или равна 3 символам:");
+    foreach (string str in filteredArray)
+    {
+        Console.WriteLine(str);
+    }
+}
